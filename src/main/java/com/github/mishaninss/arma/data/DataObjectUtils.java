c089b5fbe7fd78ff56d2b42cfa5a7472b8c9d455
Sergei Mishanin <sergei.mishanin1985@gmail.com>
2023-06-30 13:48:49 +0300

    v 3.0-SNAPSHOT

--- src/main/java/com/github/mishaninss/arma/data/DataObjectUtils.java
+++ src/main/java/com/github/mishaninss/arma/data/DataObjectUtils.java
@@ -16,120 +16,130 @@
 
 package com.github.mishaninss.arma.data;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class DataObjectUtils {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DataObjectUtils.class);
 
-    private DataObjectUtils() {
-    }
+  private static final Logger LOGGER = LoggerFactory.getLogger(DataObjectUtils.class);
 
-    public static Map<String, String> normalizeKeys(Map<String, String> data) {
-        return data.entrySet().stream()
-                .collect(Collectors.toMap(
-                        entry -> DataObject.sanitizeElementId(entry.getKey()),
-                        Map.Entry::getValue
-                ));
-    }
+  private DataObjectUtils() {
+  }
 
-    public static Map<String, Object> readDataFromObject(Iterable<String> desiredProperties, Object object) {
-        Map<String, Object> data = new LinkedHashMap<>();
-        desiredProperties.forEach(property -> {
-            Object value = readProperty(object, property);
-            if (value != null) {
-                data.put(property, value);
-            }
-        });
-        return data;
-    }
+  public static Map<String, String> normalizeKeys(Map<String, String> data) {
+    return data.entrySet().stream()
+        .collect(Collectors.toMap(
+            entry -> DataObject.sanitizeElementId(entry.getKey()),
+            Map.Entry::getValue
+        ));
+  }
 
-    public static <T> T putDataToObject(Map<String, String> data, T object) {
-        data.forEach((property, value) -> setProperty(object, property, value));
-        return object;
-    }
+  public static Map<String, Object> readDataFromObject(Iterable<String> desiredProperties,
+      Object object) {
+    Map<String, Object> data = new LinkedHashMap<>();
+    desiredProperties.forEach(property -> {
+      Object value = readProperty(object, property);
+      if (value != null) {
+        data.put(property, value);
+      }
+    });
+    return data;
+  }
 
-    public static <T> T putDataToObject(Map<String, String> data, Class<T> clazz) throws IllegalAccessException, InstantiationException {
-        T object = clazz.newInstance();
-        return putDataToObject(data, object);
-    }
+  public static <T> T putDataToObject(Map<String, String> data, T object) {
+    data.forEach((property, value) -> setProperty(object, property, value));
+    return object;
+  }
 
-    private static <T> void setProperty(T object, String property, String value) {
-        Class<?> clazz = object.getClass();
-        try {
-            Method setter = findSetter(clazz, property);
-            if (setter != null) {
-                MethodUtils.invokeMethod(object, true, setter.getName(), value);
-            } else {
-                Field field = findPropertyField(clazz, property);
-                if (field != null) {
-                    FieldUtils.writeField(field, object, value, true);
-                }
-            }
-        } catch (Exception ex) {
-            LOGGER.trace("Could not set {} property to {} data object", property, object.getClass());
+  public static <T> T putDataToObject(Map<String, String> data, Class<T> clazz)
+      throws IllegalAccessException, InstantiationException {
+    T object = clazz.newInstance();
+    return putDataToObject(data, object);
+  }
+
+  private static <T> void setProperty(T object, String property, String value) {
+    Class<?> clazz = object.getClass();
+    try {
+      Method setter = findSetter(clazz, property);
+      if (setter != null) {
+        MethodUtils.invokeMethod(object, true, setter.getName(), value);
+      } else {
+        Field field = findPropertyField(clazz, property);
+        if (field != null) {
+          FieldUtils.writeField(field, object, value, true);
         }
+      }
+    } catch (Exception ex) {
+      LOGGER.trace("Could not set {} property to {} data object", property, object.getClass());
     }
+  }
 
-    public static Object readProperty(Object object, String property) {
-        Class<?> clazz = object.getClass();
-        try {
-            Method getter = findGetter(clazz, property);
-            if (getter != null) {
-                return MethodUtils.invokeMethod(object, true, getter.getName());
-            } else {
-                Field field = findPropertyField(clazz, property);
-                if (field != null) {
-                    return FieldUtils.readField(field, object, true);
-                }
-            }
-        } catch (Exception ex) {
-            return null;
+  public static Object readProperty(Object object, String property) {
+    Class<?> clazz = object.getClass();
+    try {
+      Method getter = findGetter(clazz, property);
+      if (getter != null) {
+        return MethodUtils.invokeMethod(object, true, getter.getName());
+      } else {
+        Field field = findPropertyField(clazz, property);
+        if (field != null) {
+          return FieldUtils.readField(field, object, true);
         }
-        return null;
+      }
+    } catch (Exception ex) {
+      return null;
     }
+    return null;
+  }
 
-    private static Method findSetter(Class<?> clazz, String property) {
-        Method[] methods = clazz.getMethods();
-        String getterName = "set_" + DataObject.sanitizeElementId(property);
-        for (Method method : methods) {
-            if (StringUtils.equals(getterName, DataObject.sanitizeElementId(method.getName()))) {
-                return method;
-            }
-        }
-        return null;
+  private static Method findSetter(Class<?> clazz, String property) {
+    Method[] methods = clazz.getMethods();
+    String getterName = "set_" + DataObject.sanitizeElementId(property);
+    for (Method method : methods) {
+      if (StringUtils.equals(getterName, DataObject.sanitizeElementId(method.getName()))) {
+        return method;
+      }
     }
+    return null;
+  }
 
-    private static Method findGetter(Class<?> clazz, String property) {
-        Method[] methods = clazz.getMethods();
-        String getterName = "get_" + DataObject.sanitizeElementId(property);
-        for (Method method : methods) {
-            if (StringUtils.equals(getterName, DataObject.sanitizeElementId(method.getName()))) {
-                return method;
-            }
-        }
-        return null;
+  private static Method findGetter(Class<?> clazz, String property) {
+    Method[] methods = clazz.getMethods();
+    String getterName = "get_" + DataObject.sanitizeElementId(property);
+    for (Method method : methods) {
+      if (StringUtils.equals(getterName, DataObject.sanitizeElementId(method.getName()))) {
+        return method;
+      }
     }
+    return null;
+  }
 
-    private static Field findPropertyField(Class<?> clazz, String property) {
-        String propertyName = DataObject.sanitizeElementId(property);
-        Field[] fields = FieldUtils.getAllFields(clazz);
-        Field field = null;
-        for (Field f : fields) {
-            if (StringUtils.equals(propertyName, DataObject.sanitizeElementId(f.getName()))) {
-                field = f;
-                break;
-            }
-        }
-        return field != null && field.getType().equals(String.class) ? field : null;
+  private static Field findPropertyField(Class<?> clazz, String property) {
+    Field[] fields = FieldUtils.getAllFields(clazz);
+    Field field = null;
+    for (Field f : fields) {
+      if (StringUtils.equals(property, f.getName())) {
+        field = f;
+        break;
+      }
     }
+    if (field == null) {
+      String propertyName = DataObject.sanitizeElementId(property);
+      for (Field f : fields) {
+        if (StringUtils.equals(propertyName, DataObject.sanitizeElementId(f.getName()))) {
+          field = f;
+          break;
+        }
+      }
+    }
+    return field;
+  }
 }
